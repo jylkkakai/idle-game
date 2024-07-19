81ed4000ed8dd4bae11801204b7cde7904884f24
jylkkakai <kiitsu74@gmail.com>
2024-07-19 15:23:11 +0300

    Zoom game arena.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,21 @@
 
 const int screenWidth = 1000;
 const int screenHeight = 700;
+const int arenaWidth = 600;
+const int arenaHeight = 600;
+const int topPadding = 50;
+const int leftPadding = 50;
+
+const Vector2 gameArenaLoc = {leftPadding, topPadding};
+const Vector2 gameArenaSize = {arenaWidth, arenaHeight};
+const Rectangle arenaEdgeLine = {leftPadding, topPadding, arenaWidth,
+                                 arenaHeight};
+const Camera2D camera = {
+    {int(arenaWidth / 2) + leftPadding, int(arenaHeight / 2) + topPadding},
+    {0, 0},
+    0.0f,
+    0.1f};
+
 void UpdateDrawFrame(void);
 
 int main(void) {
@@ -21,21 +36,22 @@ int main(void) {
 
 void UpdateDrawFrame(void) {
 
-  Vector2 gameArenaLoc = {50, 50};
-  Vector2 gameArenaSize = {600, 600};
-  Camera2D camera = {{350, 350}, {0, 0}, 0.0f, 1.0f};
-
   BeginDrawing();
 
   ClearBackground(RAYWHITE);
 
-  DrawText("Idle Game", 250, 20, 20, GRAY);
+  DrawText("Idle Game", 290, 20, 20, GRAY);
   DrawRectangleV(gameArenaLoc, gameArenaSize, BLACK);
-  DrawRectangleLinesEx({50, 50, 600, 600}, 2.0f, GRAY);
+  DrawRectangleLinesEx(arenaEdgeLine, 2.0f, GRAY);
+
   BeginMode2D(camera);
-  DrawCircleLines(0, 0, 10, LIGHTGRAY);
-  // DrawCircleLines(0, 0, 200, DARKGRAY);
-  DrawRing({0, 0}, 99, 101, 0, 360, 1, DARKGRAY);
+  DrawCircleLines(0, 0, 100, LIGHTGRAY);
+  DrawCircleLines(2900, 2900, 100, LIGHTGRAY);
+  DrawCircleLines(-2900, 2900, 100, LIGHTGRAY);
+  DrawCircleLines(2900, -2900, 100, LIGHTGRAY);
+  DrawCircleLines(-2900, -2900, 100, LIGHTGRAY);
+
+  DrawRing({0, 0}, 990, 1010, 0, 360, 1, DARKGRAY);
   EndMode2D();
   EndDrawing();
 }
